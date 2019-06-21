f6b77b01946ce88d6f57b77017855a7dbead9797
chrispetito <christopherpetito@gmail.com>
2019-06-21 09:07:49 -0700

    add status code test for incomplete POST

--- games/games-router.js
+++ games/games-router.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const db = require('./games-model')
+const express = require("express");
+const db = require("./games-model");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    db.getAll().then(games => {
-        res.status(200).json(games)
+router.get("/", (req, res) => {
+  db.getAll()
+    .then(games => {
+      res.status(200).json(games);
     })
     .catch(err => {
-        res.status(500).json(err)
-    })
-})
+      res.status(500).json(err);
+    });
+});
 
-router.post('/', (req, res) => {
-    db.insert(req.body).then(()=> {
-        res.status(201).json({ message: 'Game added successfully' })
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+router.post("/", (req, res) => {
+  const { title, genre } = req.body;
+  db.insert(req.body).then(() => {
+    if (!title || !genre) {
+      res.status(422).json({ message: "Title and genre fields are required" });
+    } else {
+      res.status(201).json({ message: "Game added succesfully" });
+    }
+  })
+  .catch(err => {
+      res.status(500).json(err)
+  })
+});
 
 module.exports = router;
--- games/games.spec.js
+++ games/games.spec.js
@@ -12,13 +12,23 @@ describe('Games Model', () => {
     })
     describe('GET /', () => {
         it('should respond with a 200 status code', () => {
-            return supertest(server).get('/').expect(200)
+            return supertest(server).get('/api/games').expect(200)
         })
         it('should return JSON', () => {
-            return supertest(server).get('/').expect('content-type', /json/i)
+            return supertest(server).get('/api/games').expect('content-type', /json/i)
         })
     })
     describe('POST /', () => {
+        it('should return a 201 status code when added succesfully', async () => {
+            const game = { title: 'titleExample', genre: 'genreExample', releaseYear: 1980 }
+            const res = await supertest(server).post('/api/games').send(game)
+            expect(res.status).toBe(201)
+        })
+        it('should return a 422 if input is incomplete', async () => {
+            const game = { title: '', genre: 'genreExample', releaseYear: 1980 }
+            const res = await supertest(server).post('/api/games').send(game)
+            expect(res.status).toBe(422)
+        })
         it('should insert provided multiple games', async () => {
             await insert({ title: 'titleExample', genre: 'genreExample', releaseYear: 1980 })
             await insert({ title: 'titleExample2', genre: 'genreExample2', releaseYear: 1980 })
